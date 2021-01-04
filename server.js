5687c7acb3ca6b9f60264cb97d12d51d28ca32a9
Eric Gustin <ericgustin44@gmail.com>
2021-01-04 15:52:42 -0800

    Accepts POST and GET requests

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
-const path = require("path");
+const cors = require("cors");
+
+// middleware
+app.use(cors());
+app.use(express.json())
 
 let pageHits = 0;
+let numOfPosts = 0
 
 app.get("/", (req, res) => {
   pageHits++;
   res.send("page hits: " + pageHits);
+});
+
+app.post("/", (req, res) => {
+  numOfPosts++;
+  res.send("Total number of POST requests: " + numOfPosts);
 });
 
 app.listen(port, () => {
