87921daa54e53a9383fe3b020973000ec716ae9a
Eric Gustin <ericgustin44@gmail.com>
2021-01-04 20:10:14 -0800

    add package script

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
+
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 const proConfig = process.env.DATABASE_URL;
