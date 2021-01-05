fd0a2e8a24d900fb2c689979d04a169f4238acb0
Eric Gustin <ericgustin44@gmail.com>
2021-01-04 18:59:39 -0800

    database & env vars

--- server.js
+++ server.js
@@ -2,18 +2,40 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
 const cors = require("cors");
+const pool = require("./db");
 
 // middleware
 app.use(cors());
 app.use(express.json())
 
-let pageHits = 0;
-let numOfPosts = 0
+// let pageHits = 0;
+let numOfPosts = 0;
 
-app.get("/", (req, res) => {
-  pageHits++;
-  res.send("page hits: " + pageHits);
+app.get("/", async (req, res) => {
+  try {
+    // get the current page hits from postgreSQL
+    const allData = await pool.query("SELECT * FROM pagedata");
+    const pageHits = allData.rows[0]["pagehits"]
+
+    // update the pagehits column... should this be in its own app.put function?
+    await pool.query("UPDATE pagedata SET pagehits = $1", [pageHits+1]);
+
+    // Display the new number of page hits
+    res.send("Page hits: " + (pageHits+1));
+  } catch (err) {
+    console.log(err.message);
+  }
 });
+
+app.put("/", async (req, res) => {
+  try {
+    const pageHits = 0;
+    const updatePageHits = await pool.query("UPDATE pagedata SET pagehits = $1", [pageHits]);
+    res.json("Updated Page Hits!");
+  } catch (err) {
+    console.log(err.message);
+  }
+})
 
 app.post("/", (req, res) => {
   numOfPosts++;
