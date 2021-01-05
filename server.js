5b25f251729f91d08501d686c6d5007a5bf53c14
Eric Gustin <ericgustin44@gmail.com>
2021-01-04 19:53:57 -0800

    require db

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
 const cors = require("cors");
-const pool = require("./db.js");
+const pool = require("./db");
 
 // middleware
 app.use(cors());
