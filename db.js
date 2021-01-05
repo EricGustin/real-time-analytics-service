7e4438e2c654e799b4d65683c5b81dc6e8858a6d
Eric Gustin <ericgustin44@gmail.com>
2021-01-04 20:20:18 -0800

    ssl

--- db.js
+++ db.js
@@ -1,14 +1,19 @@
 const Pool = require("pg").Pool;
-require("dotenv").config();
+require('dotenv').config();
 
+const devConfig = {
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  port: process.env.PG_PORT,
+  ssl: true,
+};
 
-const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const proConfig = {
+  connectionString: process.env.DATABASE_URL
+}
 
-const proConfig = process.env.DATABASE_URL;
-
-const pool = new Pool({
-  connectionString:
-    proConfig,
-});
+const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig);
 
 module.exports = pool;
