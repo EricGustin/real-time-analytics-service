481a4de484203c15436e9507f20195fa13288569
Eric Gustin <ericgustin44@gmail.com>
2021-01-07 23:56:04 -0800

    display website's page hits per day

--- server.js
+++ server.js
@@ -10,21 +10,35 @@ app.use(express.json());
 
 let numOfPosts = 0;
 
+// app.get("/pagehits", async (req, res) => {
+//   try {
+//     // get the current page hits from postgreSQL
+//     const allData = await pool.query("SELECT * FROM pagedata");
+//     const pageHits = allData.rows[0]["pagehits"]
+
+//     // update the pagehits column... should this be in its own app.put function?
+//     await pool.query("UPDATE pagedata SET pagehits = $1", [pageHits+1]);
+
+//     // Display the new number of page hits
+//     res.send("Page hits: " + (pageHits+1));
+//   } catch (err) {
+//     console.log(err.message);
+//   }
+// });
 app.get("/pagehits", async (req, res) => {
   try {
-    // get the current page hits from postgreSQL
-    const allData = await pool.query("SELECT * FROM pagedata");
-    const pageHits = allData.rows[0]["pagehits"]
+    // Insert new Page_Hit event into the events table
+    await pool.query("INSERT INTO events(event_name, time_stamp) VALUES ('Page_Hit', current_timestamp)");
 
-    // update the pagehits column... should this be in its own app.put function?
-    await pool.query("UPDATE pagedata SET pagehits = $1", [pageHits+1]);
+     // get the current page hits from postgreSQL
+     const allData = await pool.query("SELECT * FROM events");
 
-    // Display the new number of page hits
-    res.send("Page hits: " + (pageHits+1));
+     res.json(allData.rows);
   } catch (err) {
     console.log(err.message);
   }
 });
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/home.html');
